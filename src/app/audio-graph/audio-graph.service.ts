a00345eb7b916691ccb5278a9f951f873cd95465
Patrick Stephansen <pstephansen89@gmail.com>
2018-12-15 14:39:45 +0200

    add more help on different nodes

--- src/app/audio-graph/audio-graph.service.ts
+++ src/app/audio-graph/audio-graph.service.ts
@@ -121,7 +121,15 @@ export class AudioGraphService {
         numberInputs: 0,
         numberOutputs: 1,
         sourceIds: [],
-        canDelete: true
+        canDelete: true,
+        helpText: `A source that emits a periodic wave.
+          Similar to a Voltage Controlled Oscillator or Low Frequency Oscillator in a physical synth.
+          Negative frequency or gain values invert the phase.
+          Detune is a frequency offset in cents, which means 100 per semi-tone ie. 1200 per octave.
+          Output gain should be between -1 and 1 for audible signals.
+          Connect directly to the output to hear a constant tone.
+          The frequency and gain parameters can be modulated by connecting them to another oscillator.
+          Try connecting a low frequency inverted sawtooth to the output gain of an audible oscillator for a percussive sound.`
       },
       [
         {
@@ -184,7 +192,10 @@ export class AudioGraphService {
         numberInputs: 1,
         numberOutputs: 1,
         sourceIds: [],
-        canDelete: true
+        canDelete: true,
+        helpText: `Multiplies each sample of the incoming signal by a factor to boost or attenuate it.
+          Similar to a Voltage Controlled Amplifier in a physical synth.
+          Negative values invert the phase of the signal.`
       },
       [
         {
@@ -218,7 +229,12 @@ export class AudioGraphService {
         numberInputs: 1,
         numberOutputs: 1,
         sourceIds: [],
-        canDelete: true
+        canDelete: true,
+        helpText: `Emits the incoming signal unchanged after the set delay time.
+          Allows feedback loops to be created in the audio graph.
+          Try connecting a percussive sound source, then feed the output to a gain node with a value between 0 and 1,
+          and connect that gain node back to the input of the delay.
+          Modulating the delay time allows phaser and chorus effects to be created.`
       },
       [
         {
@@ -257,7 +273,12 @@ export class AudioGraphService {
         numberInputs: 1,
         numberOutputs: 1,
         sourceIds: [],
-        canDelete: true
+        canDelete: true,
+        helpText: `Shapes the frequency response of the incoming signal.
+          Use it in lowpass mode to tame the upper harmonics and make sawtooth and square waves more listenable,
+          or to reduce the pop caused by their discontinuities when used at low frequencies.
+          Quality factor controls the steepness of the frequency response curve.
+          Higher values behave similarly to resonance on an analogue filter.`
       },
       [
         {
@@ -317,7 +338,10 @@ export class AudioGraphService {
         numberInputs: 1,
         numberOutputs: 1,
         sourceIds: [],
-        canDelete: true
+        canDelete: true,
+        helpText: `A simple wave shaping distortion that adds more harmonic content to boring waveforms.
+          It also makes interference between waves easier to hear.
+          Try connecting a low and a high frequency sine oscillator to the input, and the distortion output to the speakers.`
       },
       []
     ];
@@ -339,7 +363,12 @@ export class AudioGraphService {
         numberInputs: 0,
         numberOutputs: 1,
         sourceIds: [],
-        canDelete: true
+        canDelete: true,
+        helpText: `Emits a "constant" stream of samples with the value of the offset parameter.
+          It is not always constant since the offset can be modulated by other nodes.
+          Useful for adding an offset to a waveform or for controlling multiple parameters from one place.
+          It can be used together with a gain node for unit conversions eg.
+          feed it into a gain multiplier of 0.016666 to convert beats per minute to hertz.`
       },
       [
         {
