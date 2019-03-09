c93602df121118594ffeb81b4f46697e67c3d099
Patrick Stephansen <pstephansen89@gmail.com>
2019-03-09 16:40:29 +0200

    Updates

--- assets/audio-worklet-processors/noise.js
+++ assets/audio-worklet-processors/noise.js
@@ -1,27 +1,35 @@
 registerProcessor(
-  "noise",
+  'noise',
   class Noise extends AudioWorkletProcessor {
     static get parameterDescriptors() {
       return [
         {
-          name: "stepMax",
+          name: 'stepMax',
           defaultValue: 1,
           minValue: 0,
           maxValue: 1,
-          automationRate: "a-rate"
+          automationRate: 'a-rate'
         },
         {
-          name: "stepMin",
+          name: 'stepMin',
           defaultValue: 0,
           minValue: 0,
           maxValue: 1,
-          automationRate: "a-rate"
+          automationRate: 'a-rate'
+        },
+        {
+          name: 'sampleHold',
+          defaultValue: 1,
+          minValue: 1,
+          maxValue: 1000000,
+          automationRate: 'a-rate'
         }
       ];
     }
     constructor() {
       super();
       this.previousValue = 0;
+      this.samplesHeld = 0;
     }
     process(inputs, outputs, parameters) {
       // Get the first output.
@@ -32,26 +40,34 @@ registerProcessor(
       let stepMax = parameters.stepMax;
       let stepMaxLength = stepMax.length;
       let getStepMax = stepMaxLength > 1 ? i => stepMax[i] : () => stepMax[0];
+      let sampleHold = parameters.sampleHold;
+      let sampleHoldLength = sampleHold.length;
+      let getSampleHold =
+        sampleHoldLength > 1 ? i => sampleHold[i] : () => sampleHold[0];
 
-      for (let channel = 0; channel < output.length; ++channel) {
-        for (let i = 0; i < output[channel].length; ++i) {
-          const stepSize =
-            Math.random() * (getStepMax(i) - getStepMin(i)) + getStepMin(i);
-          let next;
-          const preferUp = Math.random() > 0.5;
-          if (preferUp) {
-            next =
-              this.previousValue + stepSize > 1
-                ? this.previousValue - stepSize
-                : this.previousValue + stepSize;
-          } else {
-            next =
-              this.previousValue - stepSize < -1
-                ? this.previousValue + stepSize
-                : this.previousValue - stepSize;
-          }
-          output[channel][i] = next;
+      for (let i = 0; i < output[0].length; ++i, ++this.samplesHeld) {
+        const stepSize =
+          Math.random() * (getStepMax(i) - getStepMin(i)) + getStepMin(i);
+        let next;
+        const preferUp = Math.random() > 0.5;
+        if (preferUp) {
+          next =
+            this.previousValue + stepSize > 1
+              ? this.previousValue - stepSize
+              : this.previousValue + stepSize;
+        } else {
+          next =
+            this.previousValue - stepSize < -1
+              ? this.previousValue + stepSize
+              : this.previousValue - stepSize;
+        }
+
+        if (this.samplesHeld >= getSampleHold(i)) {
+          this.samplesHeld = 0;
           this.previousValue = next;
+        }
+        for (let channel = 0; channel < output.length; ++channel) {
+          output[channel][i] = this.previousValue;
         }
       }
       return true;
