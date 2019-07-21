f2f79745b2b7e14d84e732600c7204c685ddea6e
Patrick Stephansen <pstephansen89@gmail.com>
2019-07-21 19:44:11 +0200

    test time since last state transition in envelope

--- src/audio-worklet-processors/getEnvelopeValue.js
+++ src/audio-worklet-processors/getEnvelopeValue.js
@@ -1,6 +1,7 @@
 import { getValueAtTime } from './linear-change';
 
 // ASSUMPTION: this will be called every sample, so it's safe to always advance time by one sample
+// returns the value at end of the sample, so if attack time is 0, attack value will be returned on the sample of the recieved trigger
 export const getEnvelopeValue = (
   sampleRate,
   { attackTime, attackValue, holdTime, decayTime, sustainValue, releaseTime },
@@ -10,81 +11,118 @@ export const getEnvelopeValue = (
 ) => {
   let outputStage;
   const sampleTime = 1 / sampleRate;
+  let secondsSinceStateTransitionOutput =
+    secondsSinceStateTransition + sampleTime;
   if (stage === 'rest') {
     if (triggerValue <= 0) {
       outputStage = 'rest';
     } else {
       if (sampleTime < attackTime) {
         outputStage = 'attack';
+        secondsSinceStateTransitionOutput = sampleTime;
       } else if (sampleTime - attackTime < holdTime) {
         outputStage = 'hold';
+        secondsSinceStateTransitionOutput = sampleTime - attackTime;
       } else if (sampleTime - attackTime - holdTime < decayTime) {
         outputStage = 'decay';
+        secondsSinceStateTransitionOutput = sampleTime - attackTime - holdTime;
       } else {
         outputStage = 'sustain';
+        secondsSinceStateTransitionOutput =
+          sampleTime - attackTime - holdTime - decayTime;
       }
     }
   }
   if (stage === 'attack') {
     if (triggerValue <= 0) {
-      if (releaseTime) {
+      if (sampleTime < releaseTime) {
         outputStage = 'release';
+        secondsSinceStateTransitionOutput = sampleTime;
       } else {
         outputStage = 'rest';
+        secondsSinceStateTransitionOutput = sampleTime - releaseTime;
       }
     } else {
       if (secondsSinceStateTransition + sampleTime < attackTime) {
         outputStage = 'attack';
-      } else if (secondsSinceStateTransition + sampleTime - attackTime < holdTime) {
+      } else if (
+        secondsSinceStateTransition + sampleTime - attackTime <
+        holdTime
+      ) {
         outputStage = 'hold';
+        secondsSinceStateTransitionOutput =
+          secondsSinceStateTransition + sampleTime - attackTime;
       } else if (
         secondsSinceStateTransition + sampleTime - attackTime - holdTime <
         decayTime
       ) {
         outputStage = 'decay';
+        secondsSinceStateTransitionOutput =
+          secondsSinceStateTransition + sampleTime - attackTime - holdTime;
       } else {
         outputStage = 'sustain';
+        secondsSinceStateTransitionOutput =
+          secondsSinceStateTransition +
+          sampleTime -
+          attackTime -
+          holdTime -
+          decayTime;
       }
     }
   }
   if (stage === 'hold') {
     if (triggerValue <= 0) {
-      if (releaseTime) {
+      if (sampleTime < releaseTime) {
         outputStage = 'release';
+        secondsSinceStateTransitionOutput = sampleTime;
       } else {
         outputStage = 'rest';
+        secondsSinceStateTransitionOutput = sampleTime - releaseTime;
       }
     } else {
       if (secondsSinceStateTransition + sampleTime < holdTime) {
         outputStage = 'hold';
-      } else if (secondsSinceStateTransition + sampleTime - holdTime < decayTime) {
+      } else if (
+        secondsSinceStateTransition + sampleTime - holdTime <
+        decayTime
+      ) {
         outputStage = 'decay';
+        secondsSinceStateTransitionOutput =
+          secondsSinceStateTransition + sampleTime - holdTime;
       } else {
         outputStage = 'sustain';
+        secondsSinceStateTransitionOutput =
+          secondsSinceStateTransition + sampleTime - holdTime - decayTime;
       }
     }
   }
   if (stage === 'decay') {
     if (triggerValue <= 0) {
-      if (releaseTime) {
+      if (sampleTime < releaseTime) {
         outputStage = 'release';
+        secondsSinceStateTransitionOutput = sampleTime;
       } else {
         outputStage = 'rest';
+        secondsSinceStateTransitionOutput = sampleTime - releaseTime;
       }
     } else {
       if (secondsSinceStateTransition + sampleTime < decayTime) {
         outputStage = 'decay';
       } else {
         outputStage = 'sustain';
+        secondsSinceStateTransitionOutput =
+          secondsSinceStateTransition + sampleTime - decayTime;
       }
     }
   }
   if (stage === 'sustain') {
     if (triggerValue <= 0) {
-      if (releaseTime) {
+      if (sampleTime < releaseTime) {
         outputStage = 'release';
+        secondsSinceStateTransitionOutput = sampleTime;
       } else {
         outputStage = 'rest';
+        secondsSinceStateTransitionOutput = sampleTime - releaseTime;
       }
     } else {
       outputStage = 'sustain';
@@ -96,23 +134,30 @@ export const getEnvelopeValue = (
         outputStage = 'release';
       } else {
         outputStage = 'rest';
+        secondsSinceStateTransitionOutput =
+          secondsSinceStateTransition + sampleTime - releaseTime;
       }
     } else {
-      if (attackTime) {
+      if (sampleTime < attackTime) {
         outputStage = 'attack';
-      } else if (holdTime) {
+        secondsSinceStateTransitionOutput = sampleTime;
+      } else if (sampleTime - attackTime < holdTime) {
         outputStage = 'hold';
-      } else if (decayTime) {
+        secondsSinceStateTransitionOutput = sampleTime - attackTime;
+      } else if (sampleTime - attackTime - holdTime < decayTime) {
         outputStage = 'decay';
+        secondsSinceStateTransitionOutput = sampleTime - attackTime - holdTime;
       } else {
         outputStage = 'sustain';
+        secondsSinceStateTransitionOutput =
+          sampleTime - attackTime - holdTime - decayTime;
       }
     }
   }
   return {
     stage: outputStage,
-    stageProgress: 0,
-    secondsSinceStateTransition: 0,
-    outputValue: 0
+    stageProgress: undefined,
+    secondsSinceStateTransition: secondsSinceStateTransitionOutput,
+    outputValue: undefined
   };
 };
