59d761d5c7b62840d1b7eeae579badab2e6b1521
Patrick Stephansen <pstephansen89@gmail.com>
2019-09-26 20:42:15 +0200

    more reliable worklet source maps in dev mode

--- webpack.worklets.dev.js
+++ webpack.worklets.dev.js
@@ -1,7 +1,8 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.worklets.common');
 module.exports = merge(common, {
+  devtool: 'inline-source-map',
   output: {
-    filename: '[name].js'
+    filename: '[name].js',
   }
 });
