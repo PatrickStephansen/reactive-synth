0d2ea089965a4759942294b60f48b46b9d1f03c9
Patrick Stephansen <pstephansen89@gmail.com>
2020-06-16 16:53:52 +0200

    stop tracking cache hack file that changes every time dependencies change

--- src/app/cache-hack/cache.ts
+++
@@ -1,5 +0,0 @@
-export const workletUrl = '/assets/audio-worklet-processors/worklets.js';
-export const bitcrusherWasmUrl = '/assets/audio-worklet-processors/reactive_synth_bitcrusher.8eeef0f132cc3d43289deb2fdfe9654b.wasm';
-export const bitcrusherWorkletUrl = '/assets/audio-worklet-processors/bitcrusher.81c92344a1413f5cb6f99a4b58d4f9b1.js';
-export const inverseGainWasmUrl = '/assets/audio-worklet-processors/reactive_synth_inverse_gain.c00198897ccd503445bb3343130bdd78.wasm';
-export const inverseGainWorkletUrl = '/assets/audio-worklet-processors/inverse-gain.45f3d492c9b499d79d40e9cf7cfcb3a0.js';
