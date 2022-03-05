b556e85f3556e8fa1c0df322a93c6d7fea77805c
Patrick Stephansen <pstephansen89@gmail.com>
2022-03-05 13:24:55 +0200

    fix void promise

--- src/app/audio-signal-chain/module-ready-promise.ts
+++ src/app/audio-signal-chain/module-ready-promise.ts
@@ -1,5 +1,5 @@
 export const createModuleReadyPromise = (port: MessagePort) =>
-  new Promise(resolve =>
+  new Promise<void>(resolve =>
     port.addEventListener('message', function moduleReady(event) {
       if (event.data && event.data.type === 'module-ready' && event.data.value) {
         resolve();
